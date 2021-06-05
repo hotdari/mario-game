dbeded8ca36a5d66e061071dd714ed4309b2ad00
hotdari <hotdari90@gmail.com>
2021-06-05 17:01:35 +0900

    first commit

---
+++ MarioController.js
@@ -0,0 +1,36 @@
+class Mario {
+  constructor(){
+    this.currentState = ""
+    this.x = 0;
+    this.y = 0;
+  }
+
+  left(){
+    return this.x--;
+  }
+
+  right(){
+    return this.x++;
+  }
+}
+
+const mario = new Mario();
+
+window.addEventListener("keydown", function (event) {
+  console.log(event.keyCode)
+
+  const right = event.keyCode === 39
+  const left = event.keyCode === 37
+
+  let DOM_Mario_x = "translate(calc(-50% + "+ mario.x * 8 +"px), -50%)";
+
+  if(right){
+    mario.right();
+    document.getElementById("mario").style.transform = DOM_Mario_x;
+  }
+
+  if(left){
+    mario.left();
+    document.getElementById("mario").style.transform = DOM_Mario_x;
+  }
+})
